0a977aa83254f94692cf4678c5ff2168a62b4ffa
Kermo Sutt <kermo.sutt@tptlive.ee>
2023-09-25 11:16:30 +0300

    Add task

---
+++ script.js
@@ -0,0 +1,42 @@
+//console.log(window, {document});
+// setTimeout(() => {
+//     document.location = 'https://google.com';
+// }, 5000);
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+let x = 10;
+let y = 10;
+let pressed = false;
+let arcSize = 20;
+
+let color = document.querySelector('#color');
+
+setInterval(() => {
+    if(pressed){
+        ctx.fillStyle = color.value;
+        ctx.beginPath();
+        ctx.arc(x, y, arcSize, 0, 2*Math.PI);
+        ctx.fill();
+    }
+},16);
+
+canvas.addEventListener('mousemove', event => {
+    console.log(event);
+    x = event.clientX - canvas.offsetLeft;
+    y = event.clientY - canvas.offsetTop;
+});
+canvas.addEventListener('mousedown', event => {
+    pressed = true;
+});
+canvas.addEventListener('mouseup', event => {
+    pressed = false;
+});
+let size = document.querySelector('#size');
+let sizeValue = document.querySelector('#sizeValue');
+sizeValue.innerHTML = size.value;
+arcSize = size.value;
+size.addEventListener('input', ()=> {
+    sizeValue.innerHTML = size.value;
+    arcSize = size.value;
+});
